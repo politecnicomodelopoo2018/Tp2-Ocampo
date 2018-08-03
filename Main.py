4fd9e1145cca9b6adff12c7616e7a8031f8ac827
sasas <sarasa@sasa>
2018-08-03 10:03:14 -0300

    01/8

--- Main.py
+++ Main.py
@@ -1,7 +1,67 @@
-from Base import *
-from Familia import *
-from Familiares import *
-from Asentamiento import *
-from Ocupacion import *
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+import os
 
 
+def menu():
+
+    os.system('clear')
+
+    print("Selecciona una opción")
+
+    print("\t1 - Inserts")
+
+    print("\t2 - Update")
+
+    print("\t3 - Delete")
+
+    print("\t4 - Selects")
+
+    print("\t9 - salir")
+
+
+while 1:
+
+    # Mostramos el menu
+
+    menu()
+
+    # solicituamos una opción al usuario
+
+    opcionMenu = input("inserta una opcion \n")
+
+    if opcionMenu == "1":
+
+        os.system('clear')
+
+        input("Has pulsado la opcion de Inserts...\npulsa una tecla para continuar")
+
+    elif opcionMenu == "2":
+
+        os.system('clear')
+
+        input("Has pulsado la opcion de Update...\npulsa una tecla para continuar")
+
+    elif opcionMenu == "3":
+
+        os.system('clear')
+
+        input("Has pulsado la opcion de Delete...\npulsa una tecla para continuar")
+
+    elif opcionMenu == "4":
+
+        os.system('clear')
+
+        input("Has pulsado la opcion de Selects...\npulsa una tecla para continuar")
+
+    elif opcionMenu == "9":
+
+        break
+
+    else:
+
+        os.system('clear')
+
+        input("No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")
